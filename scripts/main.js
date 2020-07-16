1dbe7c7018fbcfaa9a99f5b8ca3be492cf873a75
neteraxe <neteraxe@126.com>
2020-07-16 23:17:41 +0800

    Change

--- scripts/main.js
+++ scripts/main.js
@@ -25,15 +25,3 @@ else {
 button.onclick = function () {
     setUserName();
 }
-
-var gitalk = new Gitalk({
-    clientID: '1bbce2bcdc0813fc33ae',
-    clientSecret: '5398db17c3b4ffe0c40a960c86dac716443d4146',
-    repo: 'GitHub repo',
-    owner: 'GitHub repo owner',
-    admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
-    id: location.pathname,      // Ensure uniqueness and length less than 50
-    distractionFreeMode: false  // Facebook-like distraction free mode
-})
-
-gitalk.render('gitalk-container')
