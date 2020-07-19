af21159600ab8c92efe8cc45415fbd3f3ff17d51
neteraxe <neteraxe@126.com>
2020-07-19 22:30:34 +0800

    Reinit project

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+var randomColor = require('randomcolor'); // import the script
+var color = randomColor(); // a hex code for an attractive color
+
 let img = document.querySelector('img');
 
 let button = document.querySelector('button');
