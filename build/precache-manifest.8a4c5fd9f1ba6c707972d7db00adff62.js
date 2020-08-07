2f8a520aad0bb3d24fd1324f3537521762e5895d
neteraxe <neteraxe@126.com>
2020-08-08 00:01:15 +0800

    build finish

---
+++ build/precache-manifest.8a4c5fd9f1ba6c707972d7db00adff62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44dfe7a012f4bce900de1ef11ea7af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6723788d7a259a76fe84",
+    "url": "/static/js/2.63a7ee44.chunk.js"
+  },
+  {
+    "revision": "3fcffabe2e3e6a761b892e77d32b8e64",
+    "url": "/static/js/2.63a7ee44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001bc5cdb274ca1bbda4",
+    "url": "/static/js/main.94900002.chunk.js"
+  },
+  {
+    "revision": "e76f9ebaea7048ffd855",
+    "url": "/static/js/runtime-main.e5ea9827.js"
+  }
+]);
